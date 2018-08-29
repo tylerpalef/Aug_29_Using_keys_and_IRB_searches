90da5ded7b942d9fb1631df4a31c711848e6751f
“Tyler <tylerpalef@gmail.com>
2018-08-29 10:43:49 -0400

    Answered the four questions

---
+++ hash_exercise.rb
@@ -0,0 +1,14 @@
+# Q: What are the keys in this (top-level) hash? HINT: use the keys method.
+# A: meta and objects
+
+# Q: What is the data type of the value that goes with the first key?
+# A: Integer
+
+# Q: What is the data type of the value that goes with the last key?
+# A: String
+
+# Q: How many representatives are there in this collection?
+# 336
+
+# A: Get the URL for the photo of one representative into a variable.
+# photo_link = house[:objects].first[:photo_url]
